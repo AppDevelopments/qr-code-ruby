9a762efae1cd9da1a435abe6f3e91b8940f97166
SD-Technology <ssvanhook@yahoo.com>
2022-10-04 19:03:05 +0000

    QR Code

--- qr.rb
+++ qr.rb
@@ -1,11 +1,51 @@
-p "Hello world!"
+p "hello world"
 
-require "qrcode"
+#require "qrcode"
+
+p "Would you like to: select a URL, join a wifi network or send a text message, or exit?"
+
+user_personchoice=Array.new
+user_personchoice.push("url")
+user_personchoice.push("wifi")
+user_personchoice.push("textmessage")
+
+p user_personchoice
+
+user_personchoice=gets.chomp
+
+if user_personchoice = "url"
+  p "What url would you like to encode?"
+ urlchoice=gets.chomp
+ 
+ p qrcode=RQRCode::QRCode.new("urlchoice")
+
+ #if user_personchoice = "wifi"
+  
+  p "What is the name of the Wifi Network you would like to join?"
+     wifichoice=gets.chomp 
+      
+     p "What is the Network name?"
+      networkchoice = gets.chomp
+      
+
+  #if user_personchoice = "textmessage"
+    p "What number would you like to send the message to?"
+    textmessage=gets.chomp
+    p 
 
 
-qrcode = RQRCode::QRCode.new("wikipedia qr code")
-
-png = qrcode.as_png({ :size => 500 })
 
 
-IO.binwrite("sometext.png", png.to_s)
+
+##urlwebsite=gets.chomp
+
+
+#qrcode = RQRCode::QRCode.new("wikipedia qr code")
+
+#png = qrcode.as_png({ :size => 500 })
+
+
+#IO.binwrite("sometext.png", png.to_s)
+
+#qrcode = RQRCode::QRCode.new("https://en.wikipedia.org/wiki/QR_code")
+end
